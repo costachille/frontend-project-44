5688a75b40a366d28e93e63d26116641def94a02
Konstantin Popov <kotyapopov521@gmail.com>
2024-06-12 01:53:07 +0800

    fixed implementation of application functionality

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 
 export const askNameAndGreet = () => {
-    const name = readlineSync.guestion('May I have your name? ');
+    const name = readlineSync.question('May I have your name? ');
 
     console.log(`Hello ${name}`);
 };
