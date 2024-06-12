b8c499bc45edf0c56495f3e088745c414ecb5536
Konstantin Popov <kotyapopov521@gmail.com>
2024-06-13 02:06:46 +0800

    fixed eslint bugs

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
+import { askNameAndGreet } from '../src/cli.js';
 
-console.log("Welcome to the Brain Games!");
-
-import { askNameAndGreet } from "../src/cli.js";
+console.log('Welcome to the Brain Games!');
 
 askNameAndGreet();
