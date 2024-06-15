6912811b475cb52fd212132052d9d5c8cbb048ee
Konstantin Popov <kotyapopov521@gmail.com>
2024-06-16 01:51:04 +0800

    removing part of the code in file

--- src/utility.js
+++ src/utility.js
@@ -3,25 +3,4 @@ const getRandomNumber = (min, max) => {
     return number;
 };
 
-const randomEquasion = (num1, num2) => {
-    const arrayOfSymbols = ['+', '-', '*'];
-    const randomSymbol = arrayOfSymbols[Math.floor(Math.random() * arrayOfSymbols.length)];
-
-    if (randomSymbol === '+') {
-        return {
-            equasion: `${num1} + ${num2}`,
-            result: `${num1} + ${num2}`,
-        };
-    } if (randomSymbol === '-') {
-        return {
-            equasion: `${num1} - ${num2}`,
-            result: `${num1} - ${num2}`,
-        };
-    }
-    return {
-        equasion: `${num1} * ${num2}`,
-        result: `${num1} * ${num2}`,
-    };
-};
-
-export { getRandomNumber, randomEquasion };
+export { getRandomNumber };
