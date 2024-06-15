89a9512b2c901ae97219281ec725c285535f2721
Konstantin Popov <kotyapopov521@gmail.com>
2024-06-16 01:36:50 +0800

    implementation of logic to file

---
+++ src/games/even.js
@@ -0,0 +1,15 @@
+import gameEngine from "../gameEngine.js";
+import { getRandomNumber } from "../utility.js";
+
+const ruleOfTheGame = 'Answer "yes" if the number is even, otherwise answer "no".';
+
+const isEven = (num) => num % 2 === 0;
+const getQuestionAndAnswer = () => {
+    const question = getRandomNumber(0, 100);
+    const answer = isEven(question) ? 'yes' : 'no';
+    return [question, answer];
+};
+
+export default () => {
+    gameEngine(ruleOfTheGame, getQuestionAndAnswer);
+}
