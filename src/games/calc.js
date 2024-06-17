f5c02f8aedbd4501d7d1d0f1e1b410fa8e08f772
Konstantin Popov <kotyapopov521@gmail.com>
2024-06-17 22:45:50 +0800

    fixed app module

---
+++ src/games/calc.js
@@ -0,0 +1,13 @@
+import gameEngine from '../src/gameEngine.js';
+import { getRandomNumber, randomEquation } from '../src/utility.js';
+
+const rule = 'What is the result of the expression?';
+
+const getQuestionAndAnswer = () => {
+    const equation = randomEquation(getRandomNumber(0, 100), getRandomNumber(0, 100));
+    return [equation.equation, equation.result];
+};
+
+export default () => {
+    gameEngine(rule, getQuestionAndAnswer);
+};
