6ea05067443501931600ba3c54fa6991606959e4
Konstantin Popov <kotyapopov521@gmail.com>
2024-06-19 03:43:27 +0800

    automation is prescribed for the gcd game

---
+++ src/games/gcd.js
@@ -0,0 +1,22 @@
+import gameEngine from "../gameEngine";
+import { getRandomNumber } from "../utility";
+
+const ruleOfTheGame = 'Find the greatest common divisor of given numbers.';
+
+const answerGcd = (x, y) => {
+    if (!y) {
+        return x;
+    }
+
+    return answerGcd(y, x % y);
+};
+
+const getQuestionAndAnswer = () => {
+    const question = [getRandomNumber(0, 100), getRandomNumber(0, 100)];
+    const answer = answerGcd(...question).toString();
+    return [question.join(' '), answer];
+};
+
+export default () => {
+    gameEngine(ruleOfTheGame, getQuestionAndAnswer);
+};
