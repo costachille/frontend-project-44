a4df18a792feb5e0e2d32c417f29812006caa483
Konstantin Popov <kotyapopov521@gmail.com>
2024-06-22 02:52:20 +0800

    fixing minore errors in the code

--- src/utility.js
+++ src/utility.js
@@ -1,30 +1,30 @@
 const getRandomNumber = (min, max) => {
-    const number = Math.floor(Math.random() * (max - min + 1)) + min;
-    return number;
+  const number = Math.floor(Math.random() * (max - min + 1)) + min;
+  return number;
 };
 
 const randomEquation = (num1, num2) => {
-    const arrOfSymbols = ['+', '-', '*'];
-    const randomSymbol = arrOfSymbols[Math.floor(Math.random() * arrOfSymbols.length)];
+  const arrOfSymbols = ['+', '-', '*'];
+  const randomSymbol = arrOfSymbols[Math.floor(Math.random() * arrOfSymbols.length)];
 
-    if (randomSymbol === '+') {
-        return {
-            equation: `${num1} + ${num2}`,
-            result: `${num1 + num2}`,
-        };
-    } 
-    
-    if (randomSymbol === '-') {
-        return {
-            equation: `${num1} - ${num2}`,
-            result: `${num1 - num2}`,
-        };
-    }
-    
+  if (randomSymbol === '+') {
     return {
-            equation: `${num1} * ${num2}`,
-            result: `${num1 * num2}`,
-        };
+      equation: `${num1} + ${num2}`,
+      result: `${num1 + num2}`,
+    };
+  }
+
+  if (randomSymbol === '-') {
+    return {
+      equation: `${num1} - ${num2}`,
+      result: `${num1 - num2}`,
+    };
+  }
+
+  return {
+    equation: `${num1} * ${num2}`,
+    result: `${num1 * num2}`,
+  };
 };
 
 export { getRandomNumber, randomEquation };
