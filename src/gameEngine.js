0366c7c9381799bd0b5e6241e334f847ae794c59
Konstantin Popov <kotyapopov521@gmail.com>
2024-06-22 01:15:36 +0800

    fixing errors in the code

--- src/gameEngine.js
+++ src/gameEngine.js
@@ -3,32 +3,32 @@ import readlineSync from 'readline-sync';
 const attemptCount = 3;
 
 const greeting = () => {
-    const userName = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${userName}!`);
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
 
-    return userName;
+ return userName;
 };
 
 export default (rule, getQuestionAndAnswer) => {
-    console.log('Welcome to the Brain Games!');
-    const userName = greeting();
-    console.log(rule);
-    let attempt = 0;
+  console.log('Welcome to the Brain Games!');
+  const userName = greeting();
+  console.log(rule);
+  let attempt = 0;
 
-    while (attempt < attemptCount) {
-        const [question, correctAnswer] = getQuestionAndAnswer();
-        console.log(`Question: ${question}`);
-        const userAnswer = readlineSync.question('Your answer: ');
+  while (attempt < attemptCount) {
+    const [question, correctAnswer] = getQuestionAndAnswer();
+    console.log(`Question: ${question}`);
+    const userAnswer = readlineSync.question('Your answer: ');
 
-        if (correctAnswer != userAnswer) {
-            console.log(`"${userAnswer}" is wrong answer.\nCorrect answer was "${correctAnswer}".`);
-            console.log(`Let's try again, ${userName}!`);
-            return;
-        }
-
-        console.log('Correct!');
-        attempt += 1;
+  if (correctAnswer !== userAnswer) {
+      console.log(`"${userAnswer}" is wrong answer.\nCorrect answer was "${correctAnswer}".`);
+      console.log(`Let's try again, ${userName}!`);
+      return;
     }
 
-    console.log(`Congratulations, ${userName}!`);
+    console.log('Correct!');
+    attempt += 1;
+  }
+
+  console.log(`Congratulations, ${userName}!`);
 };
