8f8f217ccc058c2bc7bb1cbf3d54f50111b24d7a
Konstantin Popov <kotyapopov521@gmail.com>
2024-06-22 01:32:20 +0800

    fixing minore errors in the code

--- src/games/calc.js
+++ src/games/calc.js
@@ -4,10 +4,10 @@ import { getRandomNumber, randomEquation } from '../utility.js';
 const rule = 'What is the result of the expression?';
 
 const getQuestionAndAnswer = () => {
-    const equation = randomEquation(getRandomNumber(0, 100), getRandomNumber(0, 100));
-    return [equation.equation, equation.result];
+  const equation = randomEquation(getRandomNumber(0, 100), getRandomNumber(0, 100));
+  return [equation.equation, equation.result];
 };
 
 export default () => {
-    gameEngine(rule, getQuestionAndAnswer);
+  gameEngine(rule, getQuestionAndAnswer);
 };
