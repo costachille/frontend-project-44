3ad011bf64978ad6f2982360f3ab7144e23b121b
Konstantin Popov <kotyapopov521@gmail.com>
2024-06-22 01:51:58 +0800

    fixing minore errors in the code

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,22 +1,22 @@
-import gameEngine from "../gameEngine.js";
-import { getRandomNumber } from "../utility.js";
+import gameEngine from '../gameEngine.js';
+import { getRandomNumber } from '../utility.js';
 
 const ruleOfTheGame = 'Find the greatest common divisor of given numbers.';
 
 const answerGcd = (x, y) => {
-    if (!y) {
-        return x;
-    }
+  if (!y) {
+    return x;
+  }
 
-    return answerGcd(y, x % y);
+  return answerGcd(y, x % y);
 };
 
 const getQuestionAndAnswer = () => {
-    const question = [getRandomNumber(0, 100), getRandomNumber(0, 100)];
-    const answer = answerGcd(...question).toString();
-    return [question.join(' '), answer];
+  const question = [getRandomNumber(0, 100), getRandomNumber(0, 100)];
+  const answer = answerGcd(...question).toString();
+  return [question.join(' '), answer];
 };
 
 export default () => {
-    gameEngine(ruleOfTheGame, getQuestionAndAnswer);
+  gameEngine(ruleOfTheGame, getQuestionAndAnswer);
 };
