6636c91512acc967ffd1f39f2e82a74291d9c860
Konstantin Popov <kotyapopov521@gmail.com>
2024-06-22 21:44:21 +0800

    fixed function export by default

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,9 @@
 import readlineSync from 'readline-sync';
 
-export const askNameAndGreet = () => {
+function askNameAndGreet() {
   const name = readlineSync.question('May I have your name? ');
 
   console.log(`Hello, ${name}!`);
 };
+
+export default askNameAndGreet;
